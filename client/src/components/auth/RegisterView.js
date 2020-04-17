b6de02d5f2bd01386a3167c8fdaac70e470fad94
Naveed Naqi <nnaqi5534@gmail.com>
2020-04-17 18:43:43 -0400

    fixed import warnings

--- client/src/components/auth/RegisterView.js
+++ client/src/components/auth/RegisterView.js
@@ -4,8 +4,6 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 const RegisterView = (props) => {
   const [state, setState] = useState({
